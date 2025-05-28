814f86d4a8f6a081216ef0057d0a6d27db7535cd
sigesfo <ramadhan.kpj@gmail.com>
2025-05-28 18:42:54 +0700

    fix CRUD tempat and make route more simple

--- routes/web.php
+++ routes/web.php
@@ -30,25 +30,12 @@ Route::middleware(['auth'])->group(function () {
     });
 
     // Kelola barang
-    Route::prefix('barang')->name('barang.')->group(function () {
-        Route::get('/', [BarangController::class, 'index'])->name('index');
-        Route::get('/create', [BarangController::class, 'create'])->name('create');
-        Route::post('/', [BarangController::class, 'store'])->name('store');
-        Route::get('/{barang}/edit', [BarangController::class, 'edit'])->name('edit');
-        Route::put('/{barang}', [BarangController::class, 'update'])->name('update');
-        Route::delete('/{barang}', [BarangController::class, 'destroy'])->name('destroy');
-    });
-
+    Route::resource('barang', BarangController::class);
+    
     // Kelola tempat
-    Route::prefix('tempat')->name('tempat.')->group(function () {
-        Route::get('/', [TempatController::class, 'index'])->name('index');
-        Route::get('/create', [TempatController::class, 'create'])->name('create');
-        Route::post('/', [TempatController::class, 'store'])->name('store');
-        Route::get('/{barang}/edit', [TempatController::class, 'edit'])->name('edit');
-        Route::put('/{barang}', [TempatController::class, 'update'])->name('update');
-        Route::delete('/{barang}', [TempatController::class, 'destroy'])->name('destroy');
-    });
+    Route::resource('tempat', TempatController::class);
 
+    
     // Route khusus super admin
     // Route::middleware(['role:super admin'])->group(function () {
     //     Route::get('/kelola-user', [App\Http\Controllers\KelolaUserController::class, 'index'])->name('kelola-user');
